3d92102e766787687ae9334fb38cb3d6af9f6fa8
Eric Rumble <ericwrumble@gmail.com>
2021-07-02 20:07:53 -0400

    see if comments get updated

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
+resource "aws_s3_bucket" "a" {
+  bucket = "erumble-test-bucket-a"
+  acl    = "private"
+}
+
 resource "aws_s3_bucket" "b" {
-  bucket = "erumble-test-bucket"
+  bucket = "erumble-test-bucket-b"
   acl    = "private"
 }
