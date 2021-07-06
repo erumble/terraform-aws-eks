ff4eb5b8f8d77ed19e72d1a79c3853ef6e3856b4
Eric Rumble <eric.rumble@callrail.com>
2021-07-06 17:59:23 -0400

    setup eks addons

--- eks.tf
+++ eks.tf
@@ -1,10 +1,17 @@
+# A note on the way the EKS cluster is configured:
+# All the worker nodes should be in private subnets, so the AWS VPC CNI needs to be configured
+# to use an external SNAT. This is done by setting the env var `AWS_VPC_K8S_CNI_EXTERNALSNAT=true`
+# for the VPC CNI (aws-node). See https://docs.aws.amazon.com/eks/latest/userguide/external-snat.html
+# for more details.
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "17.1.0"
 
-  cluster_name    = local.eks_cluster_name
-  cluster_version = var.eks_cluster_version
-  enable_irsa     = true
+  cluster_name     = local.eks_cluster_name
+  cluster_version  = var.eks_cluster_version
+  enable_irsa      = true
+  write_kubeconfig = false
 
   vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.private_subnets
@@ -40,6 +47,65 @@ module "eks" {
   ]
 }
 
+# EKS Addons (see https://docs.aws.amazon.com/eks/latest/userguide/eks-add-ons.html)
+
+# The vpc-cni addon requires special permissions, so it gets its own IAM role
+# See https://docs.aws.amazon.com/eks/latest/userguide/cni-iam-role.html for more info
+# Also, the service name for the vpn-cni is `aws-node`, because reasons
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name             = module.eks.cluster_id
+  addon_name               = "vpc-cni"
+  addon_version            = local.eks_addon_versions.vpc_cni[var.eks_cluster_version]
+  resolve_conflicts        = "OVERWRITE"
+  service_account_role_arn = aws_iam_role.vpc_cni.arn
+}
+
+resource "aws_iam_role" "vpc_cni" {
+  assume_role_policy = data.aws_iam_policy_document.vpc_cni_arp.json
+  name               = join("-", [local.eks_cluster_name, "vpc-cni-role"])
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.vpc_cni.name
+}
+
+data "aws_iam_policy_document" "vpc_cni_arp" {
+  for_each = toset(["vpc-cni", "kube-proxy", "coredns"])
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"] # aws-node is the service name
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "kube-proxy"
+  addon_version     = local.eks_addon_versions.kube_proxy[var.eks_cluster_version]
+  resolve_conflicts = "OVERWRITE"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "coredns"
+  addon_version     = local.eks_addon_versions.coredns[var.eks_cluster_version]
+  resolve_conflicts = "OVERWRITE"
+}
+
+
+# IAM role to grant access via kubectl
 resource "aws_iam_role" "eks_admin" {
   name               = join("-", [local.eks_cluster_name, "admin"])
   assume_role_policy = data.aws_iam_policy_document.eks_admin_arp.json
--- locals.tf
+++ locals.tf
@@ -12,4 +12,25 @@ locals {
   eks_cluster_name = join("-", [var.resource_prefix, random_pet.eks_cluster_name.id])
   vpc_azs          = formatlist("%s%s", data.aws_region.current.name, var.vpc_azs)
   vpc_name         = join("-", [var.resource_prefix, "eks-vpc"])
+
+  vpc_addon_versions = {
+    coredns = {
+      "1.20" = "1.8.3"
+      "1.19" = "1.8.0"
+      "1.18" = "1.7.0"
+      "1.17" = "1.6.6"
+    }
+    kube_proxy = {
+      "1.20" = "1.20.4-eksbuild.2"
+      "1.19" = "1.19.6-eksbuild.2"
+      "1.18" = "1.18.8-eksbuild.1"
+      "1.17" = "1.17.9-eksbuild.1"
+    },
+    vpc_cni = {
+      "1.20" = "1.7"
+      "1.19" = "1.7"
+      "1.18" = "1.7"
+      "1.17" = "1.7"
+    },
+  }
 }
