1fe92c82afe7e1cc02514d025f90cbaa39b08559
Eric Rumble <eric.rumble@callrail.com>
2021-07-06 18:18:12 -0400

    versions must start with v

--- locals.tf
+++ locals.tf
@@ -13,24 +13,25 @@ locals {
   vpc_azs          = formatlist("%s%s", data.aws_region.current.name, var.vpc_azs)
   vpc_name         = join("-", [var.resource_prefix, "eks-vpc"])
 
+  # versions must start with `v` because https://github.com/hashicorp/terraform-provider-aws/blob/main/aws/resource_aws_eks_addon.go#L51
   eks_addon_versions = {
     coredns = {
-      "1.20" = "1.8.3"
-      "1.19" = "1.8.0"
-      "1.18" = "1.7.0"
-      "1.17" = "1.6.6"
+      "1.20" = "v1.8.3"
+      "1.19" = "v1.8.0"
+      "1.18" = "v1.7.0"
+      "1.17" = "v1.6.6"
     }
     kube_proxy = {
-      "1.20" = "1.20.4-eksbuild.2"
-      "1.19" = "1.19.6-eksbuild.2"
-      "1.18" = "1.18.8-eksbuild.1"
-      "1.17" = "1.17.9-eksbuild.1"
+      "1.20" = "v1.20.4-eksbuild.2"
+      "1.19" = "v1.19.6-eksbuild.2"
+      "1.18" = "v1.18.8-eksbuild.1"
+      "1.17" = "v1.17.9-eksbuild.1"
     },
     vpc_cni = {
-      "1.20" = "1.7"
-      "1.19" = "1.7"
-      "1.18" = "1.7"
-      "1.17" = "1.7"
+      "1.20" = "v1.7"
+      "1.19" = "v1.7"
+      "1.18" = "v1.7"
+      "1.17" = "v1.7"
     },
   }
 }
